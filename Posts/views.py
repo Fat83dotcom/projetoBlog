61ece9e6f5900c4faa8aedec63d4e647b890ff36
fat83dotcom <fernando.quimic@gmail.com>
2022-10-09 20:55:44 -0300

    Implemantação do Index - dos templates - parciais - static

--- Posts/views.py
+++ Posts/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from .models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name: str = 'Posts/index.html'
+    context_object_name: str = 'posts'
+    paginate_by: int = 3
+
+    def get_queryset(self):
+        return super().get_queryset().order_by('-id_post').annotate(
+            numero_comentario=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
 
 
 class PostCategoria(PostIndex):
