1f9f065c1b0fd0ef6540ab47d578e7dcd8d11511
fat83dotcom <fernando.quimic@gmail.com>
2022-10-08 21:42:28 -0300

    Criação do models do app Comentario - Configuração do admin - Migração para p DB

--- Comentarios/admin.py
+++ Comentarios/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Comentario
 
-# Register your models here.
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_comentario',
+        'nome_comentario',
+        'email_comentario',
+        'comentario_comentario',
+        'usuario_comentario',
+        'data_comentario',
+        'publicado_comentario',
+    )
+    list_editable = ('publicado_comentario', )
+    list_display_links = ('id_comentario', 'nome_comentario', )
+
+
+admin.site.register(Comentario, ComentarioAdmin)
--- Comentarios/models.py
+++ Comentarios/models.py
@@ -1,3 +1,16 @@
+from email.policy import default
 from django.db import models
+from Posts.models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Comentario(models.Model):
+    id_comentario = models.AutoField(primary_key=True)
+    nome_comentario = models.CharField(max_length=255)
+    email_comentario = models.EmailField()
+    comentario_comentario = models.TextField()
+    post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE)
+    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    data_comentario = models.DateTimeField(default=timezone.now)
+    publicado_comentario = models.BooleanField(default=False)
